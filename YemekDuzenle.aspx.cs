f361657587ab5225873ae4966c515adc7d99d320
rumeysa yakut <131006594+morirum@users.noreply.github.com>
2024-04-07 18:01:48 +0300

    Add files via upload

---
+++ YemekDuzenle.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class YemekDuzenle : System.Web.UI.Page
+{
+    sqlsinif bgl = new sqlsinif();
+    string id = "";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        id = Request.QueryString["Yemekid"];
+        if (Page.IsPostBack == false)
+        {
+
+            SqlCommand komut = new SqlCommand("SELECET * FROM Tbl_Yemekler WHERE Yemekid=@p1", bgl.baglanti());
+            komut.Parameters.AddWithValue("@p1", id);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                TextBox1.Text = dr[1].ToString();
+                TextBox2.Text = dr[2].ToString();
+                TextBox3.Text = dr[3].ToString();
+
+            }
+            bgl.baglanti().Close();
+
+        }
+
+        if (Page.IsPostBack == false)
+        {
+            SqlCommand komut3 = new SqlCommand("SELECT * FROM Tbl_Kategoriler", bgl.baglanti());
+            SqlDataReader dr3 = komut3.ExecuteReader();
+            DropDownList1.DataTextField = "KategoriAd";
+            DropDownList1.DataValueField = "Kategoriid";
+
+            DropDownList1.DataSource = dr3;
+            DropDownList1.DataBind();
+        }
+
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        SqlCommand komut = new SqlCommand("SELECT * FROM Tbl_Kategoriler SET KategoriAd = @p1, KategoriAdet = @p2 WHERE Kategoriid = @p3", bgl.baglanti());
+        komut.Parameters.AddWithValue("@p1",TextBox1.Text);
+        komut.Parameters.AddWithValue("@p2", TextBox2.Text);
+        komut.Parameters.AddWithValue("@p3",TextBox3.Text);
+        komut.Parameters.AddWithValue("@p4", DropDownList1.SelectedValue);
+        komut.Parameters.AddWithValue("@p5", id);
+        komut.ExecuteNonQuery();
+        bgl.baglanti().Close();
+
+
+
+        SqlCommand komut2 = new SqlCommand("UPDATE Tbl-Kategoriler SET KategoriAdet = KategoriAdet+1 WHERE Kategoriid = @p1", bgl.baglanti());
+        komut2.Parameters.AddWithValue("@p1",DropDownList1.SelectedValue);
+        komut2.ExecuteNonQuery();
+        bgl.baglanti().Close();
+
+    }
+}
